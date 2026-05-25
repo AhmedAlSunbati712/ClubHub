4619bbd37b69e40485164f33f61347685f2b09f9
AhmedAlSunbati712 <ahmed.m.al.sunbati.27@dartmouth.edu>
2026-05-25 00:53:21 -0400

    feat: app skeleton

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from routes import all_blueprints
+from config import ENV
+
+def create_app():
+    app = Flask(__name__)
+    
+    for blueprint in all_blueprints:
+        app.register_blueprint(blueprint)
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(port=ENV.PORT)
--- backend/config/env.py
+++ backend/config/env.py
@@ -12,13 +12,13 @@ def load_env_var(name, default = None):
     return value
 
 class ENV:
-    PORT = load_env_var("PORT", 5000)
+    PORT = int(load_env_var("PORT", 5000))
     
     DB_HOST = load_env_var("DB_HOST", "localhost")
-    DB_PORT = load_env_var("DB_PORT", 3306)
+    DB_PORT = int(load_env_var("DB_PORT", 3306))
     DB_USER = load_env_var("DB_USER")
     DB_PASSWORD = load_env_var("DB_PASSWORD")
     DB_NAME = load_env_var("DB_NAME", "clubhub")
     
     JWT_SECRET = load_env_var("JWT_SECRET")
-    JWT_EXPIRY_HOURS = load_env_var("JWT_EXPIRY_HOURS", 24)
+    JWT_EXPIRY_HOURS = int(load_env_var("JWT_EXPIRY_HOURS", 24))
